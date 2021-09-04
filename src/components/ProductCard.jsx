475e201e74bdc8e919fc8dc6378e93891eb06527
binhdao29 <binhchan0293@gmail.com>
2021-09-05 00:23:22 +0700

    fix: product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import ProductView from './ProductView';
 
 const ProductCard = props => {
+
+  const [isActive, setActive]= useState(false);
+
+  const product = props.product;
+
+  const activeProductView = () => setActive(true);
+
+  const setDisplay = (childData) => {
+    setActive(childData);
+  }
+
   return (
     <div className="product-card col-sm-6 col-md-4 col-lg-3">
+      <ProductView display={isActive} product={product} setDisplay={setDisplay} />
       <div className="card-wrapper">
         <div className="card__image">
-          <img src={props.image01} alt="" />
-          <button className="card-btn cb-btn">Quick View</button>
+          <img src={product.image01} alt="" />
+          <button 
+            className="card-btn cb-btn"
+            onClick={activeProductView}
+          >
+              Quick View
+          </button>
         </div>
         <div className="card__content">
           <div className="card__title">
-            <a href="/">{props.title}</a>
+            <a href="/">{product.title}</a>
           </div>
           <div className="card__price">
             <span className="price__old"><del>290.3</del>&nbsp;</span>
-            <span>${props.price}</span>
+            <span>${product.price}</span>
           </div>
         </div>
       </div>
@@ -24,12 +42,7 @@ const ProductCard = props => {
 }
 
 ProductCard.propTypes = {
-  image01: PropTypes.string.isRequired,
-  image02: PropTypes.string,
-  image03: PropTypes.string,
-  title: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  slug: PropTypes.string.isRequired
+  product: PropTypes.object.isRequired
 }
 
 export default ProductCard
